eeea0784a076c8c5adf963be37e3bb54293ff95a
Mihail <aizik.frost@yandex.ru>
2019-01-15 21:59:51 +0300

    item header

--- WindowsFormsApplication2/Parsers/ItemParser/ItemBuilder/IItemHeader.cs
+++ WindowsFormsApplication2/Parsers/ItemParser/ItemBuilder/IItemHeader.cs
@@ -8,27 +8,49 @@ namespace WindowsFormsApplication2.Parsers.ItemParser.ItemBuilder {
 
 
 
+    public class ItemParser : IItemParser<IItem> {
+        public IItem Parse(string rawData) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ItemBaseHeaderParser : IItemParser<IItemBaseHeader> {
+        public IItemBaseHeader Parse(string rawData) {
+            throw new NotImplementedException();
+        }
+    }
 
 
 
-    interface IItemHeader {
+    interface IItemParser<TItem> where TItem:IItem {
+        TItem Parse(string rawData);
+    }
+
+    public interface IItem {
+        bool Itendified { get; set; }
+    }
+
+    public interface IItemBaseHeader : IItem {
         ItemRarity Rarity { get; set; }
-        string BaseName { get; set; }
+        string BaseName { get; set; }        
+    }
+
+    public interface IItemHeader: IItemBaseHeader {
         string Name { get; set; }
     }
 
-    interface ICurrencyGlobalParams {
+    public interface ICurrencyGlobalParams : IItem {
         int Count { get; set; }
     }
 
-    interface IMapGlobalParams {
+    interface IMapGlobalParams : IItem {
         int Tier { get; set; }
         int ItemQuantity { get; set; }
         int ItemRarity { get; set; }
         int PackSize { get; set; }
     }
 
-    interface IMapSuffixAndPrefixBody {
+    interface IMapSuffixAndPrefixBody : IItem {
         List<string> PreffixAndSuffixes { get; set; }
     }
 
--- WindowsFormsApplication2/Parsers/ItemParser/PoeBaseItem.cs
+++ WindowsFormsApplication2/Parsers/ItemParser/PoeBaseItem.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
+using WindowsFormsApplication2.Parsers.ItemParser.ItemBuilder;
 
 namespace WindowsFormsApplication2.Parsers.ItemParser {
-    public class PoeBaseItem {
+    public class PoeBaseItem:IItem {
         public string Class { get; set; }
         public string BaseName { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
         public Point Position { get; set; }
         public List<string> Tags { get; set; }
+        public bool Itendified { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
         public PoeBaseItem() {
             Tags = new List<string>();
         }

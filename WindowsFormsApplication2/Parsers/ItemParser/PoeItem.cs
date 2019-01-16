5f0f6e8b5017c1fbc7b90acfccd0476e34e0fc42
Mihail <aizik.frost@yandex.ru>
2019-01-16 22:00:31 +0300

    iten

--- WindowsFormsApplication2/Parsers/ItemParser/PoeItem.cs
+++ WindowsFormsApplication2/Parsers/ItemParser/PoeItem.cs
@@ -3,31 +3,31 @@ using System.Windows.Forms;
 using WindowsFormsApplication2.Parsers.ItemParser.ItemBuilder;
 
 namespace WindowsFormsApplication2.Parsers.ItemParser {
-    public class PoeItem : PoeBaseItem {
-        public int SocketsCount { get; set; }
-        public ItemRarity Rarity { get; set; }
-        public string Name { get; set; }
-        PoeItem(string baseName) : base(ItemBasePreloader.GetItem(baseName)) {
+    //public class PoeItem : PoeBaseItem {
+    //    public int SocketsCount { get; set; }
+    //    public ItemRarity Rarity { get; set; }
+    //    public string Name { get; set; }
+    //    PoeItem(string baseName) : base(ItemBasePreloader.GetItem(baseName)) {
 
-        }
+    //    }
 
-        public static IItem CreateItem() {
-            try {
-                if (Clipboard.ContainsText()) {
-                    PoeItemParser poeItemParser = new PoeItemParser();
-                    ///////
-                    return null;
-                }
-                else {
-                    return null;
-                }
-            }
-            catch (Exception) {
-                return null;
-            }
+    //    public static IItem CreateItem() {
+    //        try {
+    //            if (Clipboard.ContainsText()) {
+    //                PoeItemParser poeItemParser = new PoeItemParser();
+    //                ///////
+    //                return null;
+    //            }
+    //            else {
+    //                return null;
+    //            }
+    //        }
+    //        catch (Exception) {
+    //            return null;
+    //        }
 
 
-        }
+    //    }
 
-    }
+    //}
 }
